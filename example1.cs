1da07a0b22c36c80d34329f152074b4cf4fda462
Devarla Anusha <devarlaanusha8978@gmail.com>
2023-06-24 22:14:07 +0530

    commiting example1

---
+++ example1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CABasicexamples
+{
+    class example1
+    {
+        static void Main(string[]args)
+        {
+            string Name;
+            Console.WriteLine("enter any name ");
+            Name = Console.ReadLine();
+            Console.WriteLine("the entered name is " +Name);
+            Console.WriteLine($"the entered name is {Name}");
+        }
+    }
+}
