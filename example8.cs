45d5d69a6f2c7955bce50a7e42048d56b8ecddbc
Devarla Anusha <devarlaanusha8978@gmail.com>
2023-06-24 22:35:07 +0530

    commiting example8

---
+++ example8.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CABasicexamples
+{
+    class example8   //area of a triangle
+    {
+        static void Main()
+        {
+            double  height,baase, area;
+            Console.WriteLine("enter base and height");
+            height = Convert.ToDouble(Console.ReadLine());
+            baase = Convert.ToDouble(Console.ReadLine());
+            area = 0.5 * baase * height;
+            Console.WriteLine("the area of traingle is " + area);
+            
+
+        }
+    }
+}
